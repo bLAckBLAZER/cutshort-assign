187e6a971c106b5e138e3c5e7d315636c578dc29
Omkar Jadhav <jadhavomkar85@gmail.com>
2022-08-04 16:06:39 +0530

    [feat] added Usage page

--- src/pages/Usage.js
+++ src/pages/Usage.js
@@ -1,3 +1,58 @@
-export const Usage = () => {
-  return <div>Usage</div>;
+import { useState } from "react";
+import { ButtonPrimary, Input } from "../components";
+import IndividualLogo from "../assets/individual.png";
+import GroupLogo from "../assets/group.png";
+
+export const Usage = ({ setCurrentPage }) => {
+  const [usageType, setUsageType] = useState("individual");
+
+  return (
+    <main className="mt-16 flex flex-col items-center w-full">
+      <h1 className=" text-3xl font-bold mb-4">
+        How are you planning to use Eden?
+      </h1>
+      <p className="text-slate-400">
+        We'll stremline your setup experience accordingly.
+      </p>
+      <form
+        className="flex flex-col w-1/2 mt-8"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setCurrentPage((currPage) => currPage + 1);
+        }}
+      >
+        <div className="flex gap-8">
+          <div
+            className={`flex flex-col p-4 border rounded-md flex-1 cursor-pointer ${
+              usageType === "individual" ? "border-primary" : ""
+            }`}
+            onClick={() => setUsageType("individual")}
+          >
+            <div>
+              <img src={IndividualLogo} alt="" className="mb-2" />
+            </div>
+            <div className=" font-bold mb-2 text-slate-700">For myself</div>
+            <p className="text-sm text-slate-400">
+              Write better. Think more clearly. Stay organized.
+            </p>
+          </div>
+          <div
+            className={`flex flex-col p-4 border rounded-md flex-1 cursor-pointer ${
+              usageType === "group" ? "border-primary" : ""
+            }`}
+            onClick={() => setUsageType("group")}
+          >
+            <div>
+              <img src={GroupLogo} alt="" className="mb-2" />
+            </div>
+            <div className=" font-bold mb-2 text-slate-700">With my team</div>
+            <p className="text-sm text-slate-400">
+              Wikis, doc, tasks & projects, all in one place.
+            </p>
+          </div>
+        </div>
+        <ButtonPrimary text="Create Workspace" otherClasses="mt-6 text-sm" />
+      </form>
+    </main>
+  );
 };
